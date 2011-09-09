5ac65e5747ccfb8345a96361339a192f54847731
Daniel Gröber <dxld@darkboxed.org>
2011-09-09 19:38:38 +0200

    Add networking component

---
+++ voice-net.js
@@ -0,0 +1,40 @@
+#!/usr/bin/env node
+
+var dgram = require('dgram');
+var args = process.argv.slice(0);
+var remoteHost, port = 1337;
+
+
+while(args.length > 0){
+    var arg = args.shift();
+
+    if(arg == '--host' || arg == '-h') {
+        remoteHost = args.shift();
+    } else if (arg == '--port' || arg == '-p') {
+        port = args.shift();
+    }
+}
+
+
+
+var socket = dgram.createSocket('udp4');
+
+var seq = 0;
+
+socket.on('listening', function(){
+    var address = socket.address();
+    console.error("listening " + address.address + ":" + address.port);
+
+    process.stdin.on('data', function(chunk) {
+        socket.send(chunk, 0, chunk.length, port, remoteHost);
+    });
+
+    socket.on('message', function(msg, rinfo){
+        process.stdout.write(msg);
+    });
+
+    process.stdin.resume();
+});
+
+socket.bind(parseInt(port));
+
